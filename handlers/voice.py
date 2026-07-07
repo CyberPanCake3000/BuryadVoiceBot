c4850902783f8f9a1fd110b9ee6298d38338befe
erzhena <ergatapovaer@gmail.com>
2026-07-07 21:26:33 +0300

    add voice command

--- handlers/voice.py
+++ handlers/voice.py
@@ -6,6 +6,8 @@ from aiogram.types import Message
 
 from database.mongo import Mongo
 from database.repositories.sentences import SentencesRepository
+from bson import ObjectId
+from database.repositories.voices import VoicesRepository
 
 router = Router(name="voice")
 
@@ -23,12 +25,46 @@ async def cmd_voice(message: Message, state: FSMContext, mongo: Mongo) -> None:
         return
 
     await state.set_state(VoiceState.WAIT_VOICE)
-    await state.update_data(queue=[str(d["_id"]) for d in docs], index=0)
+    await state.update_data(
+        queue=[{"id": str(d["_id"]), "text": d["text"]} for d in docs],
+        index=0,
+    )
     await message.answer(
-        f"Предложение №1:\n\n<b>{docs[0]['text']}</b>\n\nПришлите голосовое сообщение."
+        f"Озвучьте следующее предложение:\n\n<b>{docs[0]['text']}</b>\n\nПришлите голосовое сообщение."
     )
 
 
 @router.message(StateFilter(VoiceState.WAIT_VOICE), F.voice)
 async def on_voice(message: Message, state: FSMContext, mongo: Mongo) -> None:
-    ...
+    data = await state.get_data()
+    queue = data["queue"]
+    index = data["index"]
+    current = queue[index]
+
+    voice = message.voice
+    voices = VoicesRepository(mongo.db)
+    await voices.add(
+        sentence_id=ObjectId(current["id"]),
+        telegram_id=message.from_user.id,
+        file_id=voice.file_id,
+        unique_id=voice.file_unique_id,
+        duration=voice.duration,
+    )
+
+    await message.answer("Спасибо! Каждый голос важен 🙏")
+
+    index += 1
+    if index < len(queue):
+        await state.update_data(index=index)
+        nxt = queue[index]
+        await message.answer(
+            f"Предложение №{index + 1}:\n\n<b>{nxt['text']}</b>\n\nПришлите голосовое сообщение."
+        )
+    else:
+        await state.clear()
+        await message.answer("Готово! Вы озвучили все предложения. Спасибо! 🎉")
+
+
+@router.message(StateFilter(VoiceState.WAIT_VOICE))
+async def not_a_voice(message: Message) -> None:
+    await message.answer("Пожалуйста, пришлите именно голосовое сообщение 🎤")
